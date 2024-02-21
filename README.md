779303e5f1e16ab27947ccf2b83dd7addbe5a25c
Emre Deveci <23077383+emredeveci@users.noreply.github.com>
2024-02-21 17:16:40 +0300

    I added notes to the code for clarity. A readme file was also added to the project.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Flight Fare Calculator
+
+- This program calculates flight fare based on inputs such as distance, age, round trip, etc.
+- Certain age groups, and round way trips get a special discount.
+- The user input prompts loop until the user enters valid responses.
+
+## Authors
+
+- [@emredeveci](https://github.com/emredeveci)
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Boolean validKilometers = true , validAge = true, validFlightType = true;
-        Boolean roundTripDiscount = false;
 
-        int kilometers = 0, age, flightType;
+        //variable initializations
+        boolean validKilometers = true , validAge = true, validFlightType = true;
+        boolean roundTripDiscount = false;
+
+        int kilometers = 0, age = 0, flightType;
         double ageDiscount = 0;
         double flightCost;
 
+        //receive user inputs
+        //input prompts loop until valid input is entered
         while (validKilometers){
             System.out.print("Enter the flight distance in kilometers: ");
             kilometers = input.nextInt();
@@ -25,7 +29,6 @@ public class Main {
             System.out.print("Enter your age: ");
             age = input.nextInt();
             if (age >= 1) {
-                validAge = false;
                 if (age < 12) {
                     ageDiscount = 0.5;
                 } else if (age >= 12 && age <=24) {
@@ -33,6 +36,7 @@ public class Main {
                 } else if (age > 65) {
                     ageDiscount = 0.3;
                 }
+                validAge = false;
             } else {
                 System.out.println("Please enter a valid number.");
                 validAge = true;
@@ -54,17 +58,19 @@ public class Main {
             }
         }
 
+        //final calculation with the round trip discount taken into account
         if (roundTripDiscount) {
             flightCost = kilometers * 0.1;
             flightCost = (ageDiscount == 0) ? flightCost : flightCost - (flightCost * ageDiscount);
             flightCost -= flightCost * 0.2;
             flightCost *= 2;
-            System.out.println("Your flight cost is: " + flightCost);
+            System.out.println("Your flight cost is: " + flightCost + " TL");
         } else {
             flightCost = kilometers * 0.1;
             flightCost = (ageDiscount == 0) ? flightCost : flightCost - (flightCost * ageDiscount);
-            System.out.println("Your flight cost is: " + flightCost);
+            System.out.println("Your flight cost is: " + flightCost + " TL");
         }
 
+        input.close();
     }
 }
